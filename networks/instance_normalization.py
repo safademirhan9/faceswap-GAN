2e9ec613f43e14cd9f64799cc60cf675cbd04712
Safa Demirhan <72216810+safademirhan9@users.noreply.github.com>
2022-03-24 15:52:01 +0300

    keras imports changed to tensorflow.keras

--- networks/instance_normalization.py
+++ networks/instance_normalization.py
@@ -1,7 +1,7 @@
-from keras.engine import Layer, InputSpec
-from keras import initializers, regularizers, constraints
-from keras import backend as K
-from keras.utils.generic_utils import get_custom_objects
+from tensorflow.keras.engine import Layer, InputSpec
+from tensorflow.keras import initializers, regularizers, constraints
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils.generic_utils import get_custom_objects
 
 import numpy as np
 
