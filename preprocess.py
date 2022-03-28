7916699ca04bf8b75f0d3a25e43e07d8baf8c8b1
Safa Demirhan <72216810+safademirhan9@users.noreply.github.com>
2022-03-28 17:51:14 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -50,16 +50,16 @@ def process_image(input_img, info, detector, save_interval, save_path):
     return np.zeros((3,3,3))
 
 def preprocess_video(fn_input_video, fd, save_interval, save_path):
-    print(fn_input_video)
-    info = VideoInfo()
-    output = 'dummy.mp4'
-    print(fn_input_video)
+    #print(fn_input_video)
+    #info = VideoInfo()
+    #output = 'dummy.mp4'
+    #print(fn_input_video)
     clip1 = VideoFileClip(fn_input_video, audio=False)
-    print()
-    print(type(clip1))
-    clip = clip1.fl_image(lambda img: process_image(img, info, fd, save_interval, save_path))
-    print(type(clip))
-    clip.write_videofile(output, audio=False, verbose=False)
+    #print()
+    #print(type(clip1))
+    #clip = clip1.fl_image(lambda img: process_image(img, info, fd, save_interval, save_path))
+    #print(type(clip))
+    #clip.write_videofile(output, audio=False, verbose=False)
     clip1.reader.close()
 
         
